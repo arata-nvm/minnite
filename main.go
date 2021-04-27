f24fb114122774cafe871a12e3afae06ae59e87c
arata <arata04962@gmail.com>
2021-04-28 00:44:02 +0900

    :sparkles: トークン列の評価を実装した

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/alecthomas/participle/v2/lexer"
 	"github.com/alecthomas/participle/v2/lexer/stateful"
@@ -14,21 +15,20 @@ func main() {
 }
 
 func repl() {
-	var vars [256]int
-	for i := 0; i < 10; i += 1 {
-		vars['0'+i] = i
-	}
+	vars := map[string]int{}
 
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("> ")
 		line, _ := reader.ReadString('\n')
 		tokens := tokenize(line)
-		fmt.Printf("%+v\n", tokens)
+		eval(tokens, vars)
 	}
 }
 
+// 文字列を受け取り、トークン列にする
 func tokenize(s string) []lexer.Token {
+	// トークンの定義
 	lexerDef := stateful.MustSimple([]stateful.Rule{
 		{`Ident`, `[a-zA-Z][a-zA-Z_\d]*`, nil},
 		{`Number`, `\d+`, nil},
@@ -36,6 +36,7 @@ func tokenize(s string) []lexer.Token {
 		{"whitespace", `[\n\r\s]+`, nil},
 	})
 
+	// トークンをスライスに保持する
 	var tokens []lexer.Token
 	l, _ := lexerDef.LexString("", s)
 	for {
@@ -49,31 +50,66 @@ func tokenize(s string) []lexer.Token {
 	return tokens
 }
 
-func eval(s string, vars []int) {
-	for i := 0; i < len(s); i += 1 {
-		if s[i] == ';' || s[i] == '\n' {
-			i += 1
-			continue
-		}
+// トークン列を受け取り、それを評価する
+func eval(tokens []lexer.Token, vars map[string]int) {
+	i := 0
 
-		if s[i+1] == '=' && s[i+3] == ';' {
-			name := s[i]
-			value := vars[s[i+2]]
+	// EOF=文字列の終わりまで繰り返す
+	for !tokens[i].EOF() {
+
+		// 文の最初の文字で分岐する
+		switch tokens[i].String() {
+		// let文
+		case "let":
+			name := tokens[i+1].String()
+			value := evalValue(tokens[i+3], vars)
+
+			if tokens[i+4].String() == "+" {
+				value += evalValue(tokens[i+5], vars)
+			}
+
 			vars[name] = value
-		} else if s[i+1] == '=' && s[i+3] == '+' && s[i+5] == ';' {
-			name := s[i]
-			v1 := vars[s[i+2]]
-			v2 := vars[s[i+4]]
-			vars[name] = v1 + v2
+
+		// print文
+		case "print":
+			value := evalValue(tokens[i+1], vars)
+			fmt.Printf("%d\n", value)
+
+		// それ以外はエラーとする
+		default:
+			fmt.Println("エラー")
+			return
 		}
 
-		if s[i] == '?' {
-			value := s[i+1]
-			fmt.Printf("%d\n", vars[value])
-		}
-
-		for s[i] != ';' {
+		// セミコロンの次まで進める
+		for {
 			i += 1
+			if tokens[i].String() == ";" {
+				i += 1
+				break
+			}
 		}
 	}
+}
+
+// トークンを受けとり、数値にして返す
+func evalValue(token lexer.Token, vars map[string]int) int {
+	var (
+		ident  rune = -2
+		number rune = -3
+	)
+
+	// 変数ならばその値を取得する
+	if token.Type == ident {
+		return vars[token.String()]
+	}
+
+	// 数字なら数値に変換する
+	if token.Type == number {
+		value, _ := strconv.Atoi(token.String())
+		return value
+	}
+
+	// 変数・数字以外はエラーとする
+	panic("エラー")
 }
